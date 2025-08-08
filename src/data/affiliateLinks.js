732c592af5169d2047f3e2192eee497cca323858
Ricky Andrian <ricky.andrian@primecash.co.id>
2025-08-08 09:20:21 +0700

    Adding new product

--- src/data/affiliateLinks.js
+++ src/data/affiliateLinks.js
@@ -1,13 +1,15 @@
 import cardiganRajutFloriaAbstract from '../assets/products/cardigan-rajut-floria-abstract.jpg'
-import cardiganRajutPolos from '../assets/products/cardigan-rajut-polos.jpg'
 import daisyBloomCrochet from '../assets/products/daisy-bloom-crochet.jpg'
+import floralWhimsy from '../assets/products/floral-whimsy.jpg'
+import sailorKnitStripe from '../assets/products/sailor-knit-stripe.jpg'
+import velloraCardigan from '../assets/products/vellora-cardigan.jpg'
 import vineaKnit from '../assets/products/vinea-knit.jpg'
 
 export const affiliateIcons = [
   {
-    name: 'Cardigan Rajut Floria Abstract',
-    url: 'https://s.shopee.co.id/10sNvO6cpp',
-    image: cardiganRajutFloriaAbstract
+    name: 'Vellora Cardigan',
+    url: 'https://s.shopee.co.id/2LNlVBcs9U',
+    image: velloraCardigan
   },
   {
     name: 'Daisy Bloom Crochet',
@@ -15,13 +17,23 @@ export const affiliateIcons = [
     image: daisyBloomCrochet
   },
   {
-    name: 'Cardigan Rajut Polos',
-    url: 'https://s.shopee.co.id/2LNlVBcs9U',
-    image: cardiganRajutPolos
+    name: 'Cardigan Rajut Floria Abstract',
+    url: 'https://s.shopee.co.id/10sNvO6cpp',
+    image: cardiganRajutFloriaAbstract
   },
   {
     name: 'Vinea Knit',
     url: 'https://s.shopee.co.id/Vw7I07hnq',
     image: vineaKnit
+  },
+  {
+    name: 'Floral Whimsy',
+    url: 'https://s.shopee.co.id/6AaezrgC68',
+    image: floralWhimsy
+  },
+  {
+    name: 'Sailor Knit Stripe',
+    url: 'https://s.shopee.co.id/BJRqnOfHn',
+    image: sailorKnitStripe
   }
 ]
