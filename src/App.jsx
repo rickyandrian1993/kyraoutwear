4f7fb00581672c677b822a2d7cfc56ad1315b782
Ricky Andrian <ricky.andrian@primecash.co.id>
2025-08-25 10:43:38 +0700

    Change button behavior and adding whatsapp number

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,32 @@ import avatar from './assets/logo.jpg'
 import AffiliateLinks from './components/AffiliateLinks'
 
 function App() {
+  const phoneNumber = '6283848307755' // use international format without "+"
+  const message = 'Hello, Saya !'
+
+  const openWhatsApp = () => {
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      message
+    )}`
+    window.open(url, '_blank')
+  }
+
+  const openInstagram = () => {
+    const url = `https://www.instagram.com/kyra.outwear/`
+    window.open(url, '_blank')
+  }
+
+  const openShopee = () => {
+    // const url = `https://s.shopee.co.id/zifEsxug`
+    const url = `https://shopee.co.id/kyra.outwear`
+    window.open(url, '_blank')
+  }
+
+  const openTikTok = () => {
+    const url = `https://www.tiktok.com/@kyra.outwear`
+    window.open(url, '_blank')
+  }
+
   return (
     <div
       className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat bg-fixed p-4"
@@ -22,30 +48,18 @@ function App() {
         <h1 className="text-xl font-semibold">KYRA OUTWEAR</h1>
         <p className="text-sm text-gray-500">Giving clothing a second life</p>
         <div className="flex justify-center align-middle space-x-6 m-4 text-black-600">
-          <a
-            className="text-lg"
-            href="https://www.instagram.com/kyra.outwear/"
-            target="_blank"
-          >
+          <button className="text-lg" onClick={openInstagram}>
             <FaInstagram />
-          </a>
-          <a
-            className="text-lg"
-            href="https://www.tiktok.com/@kyra.outwear"
-            target="_blank"
-          >
+          </button>
+          <button className="text-lg" onClick={openTikTok}>
             <FaTiktok />
-          </a>
-          <a className="text-lg">
+          </button>
+          <button className="text-lg" onClick={openWhatsApp}>
             <FaWhatsapp />
-          </a>
-          <a
-            className="text-lg"
-            href="https://s.shopee.co.id/zifEsxug"
-            target="_blank"
-          >
+          </button>
+          <button className="text-lg" onClick={openShopee}>
             <FaShop />
-          </a>
+          </button>
         </div>
         <div className="space-y-3">
           {/* <a className="block w-full bg-white py-2 rounded-lg font-medium shadow hover:bg-gray-100">
